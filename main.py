33f5050dcf925fb2f30935e3e7e0045e00285083
Auvrel <Moody.Elashry@outlook.com>
2025-11-06 16:42:31 +0200

    Removed all files from the repository

--- main.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-# Import the main application logic from flet_app.py
-from gui.flet_app import main
-
-if __name__ == "__main__":
-    # The 'target' function for ft.app() is now imported from flet_app.py
-    ft.app(target=main)
